3abf144c1f209993ece38831b656a9829427f961
Árpád Goretity <h2co3@h2co3.org>
2017-04-26 07:39:41 +0200

    dispatch specific parsers of top-level declarations

--- src/parser.rs
+++ src/parser.rs
@@ -6,13 +6,14 @@
 // on 07/04/2017
 //
 
+use std::slice;
 use lexer::Token;
 use ast::{ Node, NodeValue };
 
 
 #[allow(missing_debug_implementations)]
 struct Parser<'a> {
-    tokens: &'a [Token<'a>],
+    tokens: slice::Iter<'a, Token<'a>>,
 }
 
 #[derive(Debug)]
@@ -29,9 +30,19 @@ pub fn parse<'a>(tokens: &'a [Token]) -> ParseResult<'a> {
     Parser::new(tokens).parse()
 }
 
+fn error_result<'a>(begin: &'a Token, end: &'a Token, message: String) -> ParseResult<'a> {
+    Err(
+        ParseError {
+            begin:   begin,
+            end:     end,
+            message: message,
+        }
+    )
+}
+
 impl<'a> Parser<'a> {
     fn new(tokens: &'a [Token]) -> Parser<'a> {
-        Parser { tokens: tokens }
+        Parser { tokens: tokens.iter() }
     }
 
     fn has_tokens(&self) -> bool {
@@ -40,8 +51,8 @@ impl<'a> Parser<'a> {
 
     fn parse(mut self) -> ParseResult<'a> {
         let mut children = vec![];
-        let first_token = self.tokens.first();
-        let last_token = self.tokens.last();
+        let first_token = self.tokens.as_slice().first();
+        let last_token = self.tokens.as_slice().last();
 
         while self.has_tokens() {
             children.push(try!(self.parse_toplevel()));
@@ -57,6 +68,36 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_toplevel(&mut self) -> ParseResult<'a> {
+        let token = self.tokens.as_slice().first().expect(
+            "parse_toplevel() called with empty input"
+        );
+        match token.value {
+            "struct" => self.parse_struct(),
+            "class"  => self.parse_class(),
+            "enum"   => self.parse_enum(),
+            "fn"     => self.parse_function(),
+            _        => error_result(
+                token,
+                token,
+                format!(
+                    "expected struct, class, enum or function declaration; found '{}'", token.value)
+            ),
+        }
+    }
+
+    fn parse_struct(&mut self) -> ParseResult<'a> {
+        unimplemented!()
+    }
+
+    fn parse_class(&mut self) -> ParseResult<'a> {
+        unimplemented!()
+    }
+
+    fn parse_enum(&mut self) -> ParseResult<'a> {
+        unimplemented!()
+    }
+
+    fn parse_function(&mut self) -> ParseResult<'a> {
         unimplemented!()
     }
 }
