608336d6b468159590f61d6fcff117c896311992
Árpád Goretity <h2co3@h2co3.org>
2017-06-05 15:50:40 +0200

    started implementing the actual declaration generator

--- src/declgen/go.rs
+++ src/declgen/go.rs
@@ -7,12 +7,28 @@
 //
 
 use std::io;
+use std::fmt::Debug;
 use codegen::*;
 use sqir::*;
 
 
 pub fn generate_pod(sqir: &SQIR, params: &CodegenParams, wp: &mut WriterProvider) -> io::Result<()> {
-    unimplemented!()
+    fn err<E: Debug>(error: E) -> io::Error {
+        io::Error::new(io::ErrorKind::Other, format!("{:#?}", error))
+    }
+
+    let fptr = wp("PHiLe_decls.go");
+    let file = fptr.try_borrow_mut().map_err(err)?;
+
+    for (name, typ) in &sqir.named_types {
+        match *typ.borrow().map_err(err)? {
+            Type::Struct(ref st) => (),
+            Type::Class(ref ct) => (),
+            _ => (),
+        }
+    }
+
+    Ok(())
 }
 
 pub fn generate_active_record(sqir: &SQIR, params: &CodegenParams, wp: &mut WriterProvider) -> io::Result<()> {
