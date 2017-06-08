c325641811f1b948a0646169283ae314dbb54be3
Árpád Goretity <h2co3@h2co3.org>
2017-06-08 18:37:10 +0200

    started laying out schemagen

---
+++ src/schemagen/mongodb/js.rs
@@ -0,0 +1,16 @@
+//
+// schemagen/mongodb/js.rs
+// The PHiLe Compiler
+//
+// Created by Arpad Goretity (H2CO3)
+// on 08/06/2017
+//
+
+use std::io;
+use codegen::*;
+use sqir::*;
+
+
+pub fn generate(sqir: &SQIR, params: &CodegenParams, wp: &mut WriterProvider) -> io::Result<()> {
+    unimplemented!()
+}
