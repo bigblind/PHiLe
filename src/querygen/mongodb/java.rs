c4d4edabdcb46a58f45606facfe920b47c6e6468
Árpád Goretity <h2co3@h2co3.org>
2017-07-09 14:07:36 +0200

    Laying the ground for querygen

---
+++ src/querygen/mongodb/java.rs
@@ -0,0 +1,16 @@
+//
+// querygen/mongodb/java.rs
+// The PHiLe Compiler
+//
+// Created by Arpad Goretity (H2CO3)
+// on 09/07/2017
+//
+
+use std::io;
+use codegen::*;
+use sqir::*;
+
+
+pub fn generate(_sqir: &SQIR, _params: &CodegenParams, _wp: &mut WriterProvider) -> io::Result<()> {
+    unimplemented!()
+}
