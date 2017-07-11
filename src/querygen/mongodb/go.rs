d4635aaa0793c79bdd33f000fb92d94c5bc27156
Árpád Goretity <h2co3@h2co3.org>
2017-07-11 13:02:59 +0200

    generalize writing globals, even though currently only functions are supported

--- src/querygen/mongodb/go.rs
+++ src/querygen/mongodb/go.rs
@@ -16,21 +16,34 @@ pub fn generate(sqir: &SQIR, params: &CodegenParams, wp: &mut WriterProvider) ->
         for (raw_name, expr) in namespace {
             let namespace_or = |default| ns_name.as_ref().map_or(default, String::as_ref);
             let file_name = namespace_or("PHiLe-Query").to_owned() + ".go";
-            let prefixed_name = format!("{}_{}", namespace_or(""), raw_name);
+            let prefixed_name = namespace_or("").to_owned() + "_" + raw_name;
             let name = transform_func_name(&prefixed_name, params);
             let wptr = wp(&file_name)?;
             let mut wr = wptr.borrow_mut();
 
-            match expr.borrow()?.value {
-                Value::Function(ref func) => write_function(&mut *wr, &name, func)?,
-                _ => unreachable!("Non-Function global?!"),
-            }
+            write_global(&mut *wr, &name, &*expr.borrow()?)?;
         }
     }
 
     Ok(())
 }
 
-fn write_function(wr: &mut io::Write, name: &str, func: &Function) -> io::Result<()> {
+fn write_global(wr: &mut io::Write, name: &str, expr: &Expr) -> io::Result<()> {
+    let typtr = expr.ty.borrow()?;
+
+    let ty = match *typtr {
+        Type::Function(ref ty) => ty,
+        _ => unreachable!("Non-Function global?!"),
+    };
+
+    let func = match expr.value {
+        Value::Function(ref func) => func,
+        _ => unreachable!("Non-Function global?!"),
+    };
+
+    write_function(wr, name, ty, func)
+}
+
+fn write_function(wr: &mut io::Write, name: &str, ty: &FunctionType, func: &Function) -> io::Result<()> {
     unimplemented!()
 }
