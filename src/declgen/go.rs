ee4095a22d969fa73ce2faee12eeee0f620b14be
Árpád Goretity <h2co3@h2co3.org>
2017-07-12 06:43:37 +0200

    Factored out type writer in order to make it publicly available

--- src/declgen/go.rs
+++ src/declgen/go.rs
@@ -15,12 +15,24 @@ use sqir::*;
 use util::*;
 
 
+struct TypeWriter<'a> {
+    params: &'a CodegenParams,
+}
+
 struct Generator<'a> {
     sqir:   &'a SQIR,
     params: &'a CodegenParams,
     wp:     &'a mut WriterProvider,
 }
 
+
+pub fn write_type(wr: &mut io::Write, ty: &WkType, params: &CodegenParams) -> io::Result<()> {
+    TypeWriter { params }.write_type(wr, ty)
+}
+
+//
+// Plain Old Data access mode
+//
 
 pub fn generate_pod(sqir: &SQIR, params: &CodegenParams, wp: &mut WriterProvider) -> io::Result<()> {
     Generator { sqir, params, wp }.generate_pod()
@@ -32,6 +44,67 @@ pub fn generate_pod(sqir: &SQIR, params: &CodegenParams, wp: &mut WriterProvider
 
 pub fn generate_active_record(_sqir: &SQIR, _params: &CodegenParams, _wp: &mut WriterProvider) -> io::Result<()> {
     unimplemented!()
+}
+
+//
+// Generic Type Writers
+//
+
+impl<'a> TypeWriter<'a> {
+    fn write_type(&self, wr: &mut io::Write, ty: &WkType) -> io::Result<()> {
+        let rc = ty.as_rc()?;
+        let ptr = rc.borrow()?;
+
+        match *ptr {
+            Type::Bool  => write!(wr, "bool"),
+            Type::Int   => write!(wr, "int64"),
+            Type::Float => write!(wr, "float64"),
+            Type::Decimal { .. } => unimplemented!(),
+
+            Type::String => write!(wr, "string"),
+            Type::Blob   => write!(wr, "[]byte"),
+            Type::Date   => write!(wr, "time.Time"),
+
+            Type::Optional(ref wrapped) => self.write_optional_type(wr, wrapped),
+            Type::Pointer(ref pointed)  => self.write_pointer_type(wr, pointed),
+            Type::Array(ref element)    => self.write_array_type(wr, element),
+            Type::Tuple(ref types)      => self.write_tuple_type(wr, types),
+
+            // Respect type name transform
+            Type::Enum(ref et)   => write!(wr, "{}", transform_type_name(&et.name, self.params)),
+            Type::Struct(ref st) => write!(wr, "{}", transform_type_name(&st.name, self.params)),
+            Type::Class(ref ct)  => write!(wr, "{}", transform_type_name(&ct.name, self.params)),
+
+            Type::Function(_) => unimplemented!(),
+            Type::Placeholder { ref name, kind } => unreachable!("Unresolved Placeholder({}, {:#?})", name, kind),
+        }
+    }
+
+    fn write_optional_type(&self, wr: &mut io::Write, wrapped: &WkType) -> io::Result<()> {
+        self.write_pointer_type(wr, wrapped)
+    }
+
+    fn write_pointer_type(&self, wr: &mut io::Write, pointed: &WkType) -> io::Result<()> {
+        write!(wr, "*").and_then(|_| self.write_type(wr, pointed))
+    }
+
+    fn write_array_type(&self, wr: &mut io::Write, element: &WkType) -> io::Result<()> {
+        write!(wr, "[]").and_then(|_| self.write_type(wr, element))
+    }
+
+    fn write_tuple_type(&self, wr: &mut io::Write, types: &[WkType]) -> io::Result<()> {
+        write!(wr, "struct {{ ")?;
+
+        for (idx, typ) in types.iter().enumerate() {
+            write!(wr, "F{} ", idx)?;
+            self.write_type(wr, typ)?;
+            write!(wr, "; ")?;
+        }
+
+        write!(wr, "}}")?;
+
+        Ok(())
+    }
 }
 
 //
@@ -112,7 +185,7 @@ impl<'a> Generator<'a> {
         // Indexing `pad` with grapheme counts is safe because it's ASCII-only.
         for (fname, ftype) in transformed_fields {
             write!(wr, "    {}{} ", fname, &pad[grapheme_count(&fname)..])?;
-            self.write_type(wr, ftype)?;
+            write_type(wr, ftype, &self.params)?;
             writeln!(wr)?;
         }
 
@@ -165,64 +238,6 @@ impl<'a> Generator<'a> {
         }
 
         writeln!(wr, ")\n")?;
-
-        Ok(())
-    }
-
-    //
-    // Generic Type Writers
-    //
-    fn write_type(&self, wr: &mut io::Write, typ: &WkType) -> io::Result<()> {
-        let rc = typ.as_rc()?;
-        let ptr = rc.borrow()?;
-
-        match *ptr {
-            Type::Bool  => write!(wr, "bool"),
-            Type::Int   => write!(wr, "int64"),
-            Type::Float => write!(wr, "float64"),
-            Type::Decimal { .. } => unimplemented!(),
-
-            Type::String => write!(wr, "string"),
-            Type::Blob   => write!(wr, "[]byte"),
-            Type::Date   => write!(wr, "time.Time"),
-
-            Type::Optional(ref wrapped) => self.write_optional_type(wr, wrapped),
-            Type::Pointer(ref pointed)  => self.write_pointer_type(wr, pointed),
-            Type::Array(ref element)    => self.write_array_type(wr, element),
-            Type::Tuple(ref types)      => self.write_tuple_type(wr, types),
-
-            // Respect type name transform
-            Type::Enum(ref et)   => write!(wr, "{}", transform_type_name(&et.name, self.params)),
-            Type::Struct(ref st) => write!(wr, "{}", transform_type_name(&st.name, self.params)),
-            Type::Class(ref ct)  => write!(wr, "{}", transform_type_name(&ct.name, self.params)),
-
-            Type::Function(_) => unimplemented!(),
-            Type::Placeholder { ref name, kind } => unreachable!("Unresolved Placeholder({}, {:#?})", name, kind),
-        }
-    }
-
-    fn write_optional_type(&self, wr: &mut io::Write, wrapped: &WkType) -> io::Result<()> {
-        self.write_pointer_type(wr, wrapped)
-    }
-
-    fn write_pointer_type(&self, wr: &mut io::Write, pointed: &WkType) -> io::Result<()> {
-        write!(wr, "*").and_then(|_| self.write_type(wr, pointed))
-    }
-
-    fn write_array_type(&self, wr: &mut io::Write, element: &WkType) -> io::Result<()> {
-        write!(wr, "[]").and_then(|_| self.write_type(wr, element))
-    }
-
-    fn write_tuple_type(&self, wr: &mut io::Write, types: &[WkType]) -> io::Result<()> {
-        write!(wr, "struct {{ ")?;
-
-        for (idx, typ) in types.iter().enumerate() {
-            write!(wr, "Field{} ", idx)?;
-            self.write_type(wr, typ)?;
-            write!(wr, "; ")?;
-        }
-
-        write!(wr, "}}")?;
 
         Ok(())
     }
